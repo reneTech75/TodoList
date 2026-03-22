80dfb64f1696d3b10a2563e41ece46b9c9c1773d
Rene Ashu <reneTech75@gmail.com>
2026-03-22 08:14:05 -0400

    this is the first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import positional_variables as pos
+
+class Todo:
+    def __init__(self, id, description, status, createdAt, updatedAt):
+        self.id = id
+        self.description = description
+        self.status = status
+        self.createdAt = createdAt
+        self.updatedAt = updatedAt
