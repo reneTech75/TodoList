1e8aacac69310c3236978ccc19847f245221c283
Rene Ashu <reneTech75@gmail.com>
2026-04-01 22:58:19 -0400

    wrote logic for adding new task, changing the status of a task

--- task_cli.py
+++ task_cli.py
@@ -3,6 +3,23 @@ import os #we need this to check if the tasks file exists and to create it if it
 import sys #we need this because we shall be using command line arguments to add, list, and delete tasks
 from datetime import datetime #we need this to add timestamps to our tasks
 
+#sys.argv is a list in Python, which contains the command-line arguments passed to the script.
+# The first element, sys.argv[0], is the name of the script itself. The subsequent elements are the arguments passed to the script.
+# For example, if you run the script like this:
+# python task_cli.py add "Buy groceries"
+# Then sys.argv will be:
+# ['task_cli.py', 'add', 'Buy groceries']
+# In this case, sys.argv[1] will be 'add' and sys.argv[2] will be 'Buy groceries'.
+#sys.argv[1] will be the command (add, list, delete, update) and sys.argv[2] will be the task description or task id depending on the command.
+#sys.argv[3] will be the new description for the update command.
+# We will use these command line arguments to determine what action to take (add, list, delete, update) and to get the necessary information for that action (task description or task id).
+
+
+
+
+
+
+
 FILE_NAME = "tasks.json" #this is the name of the file where we will store our tasks
 
 #----------------FILE HANDLING FUNCTIONS----------------#
@@ -32,6 +49,40 @@ def update_task(task_id, new_description):
             task["updatedAt"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S") #we update the timestamp of the task to the current time
             save_task(tasks)
             print("Task updated successfully.")
+
+def delete_task(task_id):
+    tasks = load_task()
+    #tasks = [task for task in tasks if task["id"] != task_id] #we create a new list of tasks that does not include the task with the given id
+    for task in tasks:
+        if task["id"] == task_id:
+            tasks.remove(task) #we remove the task with the given id from the list of tasks
+            save_task(tasks)
+            print("Task deleted successfully.") 
+
+def change_status(task_id, status):
+    tasks = load_task()
+    for task in tasks:
+        if task["id"] == task_id:
+            task["status"] = status
+            task["updatedAt"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S") #we update the timestamp of the task to the current time
+            save_task(tasks)
+            print("Task status updated successfully.")
+            return
+    print("Task not found.") #if we do not find the task with the given id, we print this message
+    
+def add_task(description):
+    tasks = load_task()
+    task_id = len(tasks) + 1 #we generate a new id for the task by taking the length of the current list of tasks and adding 1 to it
+    new_task = {
+        "id": task_id,
+        "description": description,
+        "status": "pending", #we set the default status of a new task to pending
+        "createdAt": datetime.now().strftime("%Y-%m-%d %H:%M:%S"), #we add a timestamp to the task when it is created
+        "updatedAt": datetime.now().strftime("%Y-%m-%d %H:%M:%S") #we also add a timestamp for when the task is updated, which will be the same as the createdAt timestamp when the task is first created
+    }
+    tasks.append(new_task) #we add the new task to the list of tasks
+    save_task(tasks) #we save the updated list of tasks to the json file
+    print("Task added successfully.")
 
 
 #Main function that will be called when the script is run.
